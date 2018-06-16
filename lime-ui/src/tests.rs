781fd7d799e8eff35869bc51e71b46ba958fdf88
Andrew Hickman <andrew.hickman1@sky.com>
2018-06-16 22:56:19 +0100

    don't set panic handler in tests

--- lime-ui/src/tests.rs
+++ lime-ui/src/tests.rs
@@ -1,5 +1,3 @@
-use std::panic;
-
 use render::ScreenDimensions;
 use shrev::EventChannel;
 use specs::prelude::*;
@@ -8,7 +6,6 @@ use super::*;
 
 pub fn init_test(dims: ScreenDimensions) -> (World, Dispatcher<'static, 'static>) {
     env_logger::try_init().ok();
-    panic::set_hook(Box::new(utils::panic_hook));
 
     let mut world = World::new();
     world.add_resource(dims);
