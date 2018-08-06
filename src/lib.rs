321f859d73f6a7d4c7ac782c285ddec7806fe304
Andrew Hickman <andrew.hickman1@sky.com>
2018-08-06 19:41:42 +0100

    revert adding physics

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,3 @@
 pub extern crate lime_main_loop as main_loop;
-pub extern crate lime_physics as physics;
 pub extern crate lime_render as render;
 pub extern crate lime_ui as ui;
-pub extern crate lime_utils as utils;
